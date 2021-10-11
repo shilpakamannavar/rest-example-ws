3e7c448e5bd3e46437d5a640782fadec7dbd51ec
Shilpa Kamannavar-Patil <shilpa.kamannavar@gmail.com>
2021-10-11 18:52:03 +0530

    initial commit

---
+++ rest-example-ws/src/test/java/com/example/rest/ws/RestExampleWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExampleWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

31d1141d05308b3c1c3ac73086e7780f7fec12f9
shilpakamannavar <shilpa.kamannavar@gmail.com>
2021-10-13 11:46:19 +0000

    Update RestExampleWsApplication.java
    CHANGES made to make it run on tomcat

--- rest-example-ws/src/main/java/com/example/rest/ws/RestExampleWsApplication.java
+++ rest-example-ws/src/main/java/com/example/rest/ws/RestExampleWsApplication.java
@@ -2,12 +2,19 @@ package com.example.rest.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestExampleWsApplication {
+public class RestExampleWsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestExampleWsApplication.class, args);
 	}
+	
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(RestExampleWsApplication.class);
+	  }
 
 }

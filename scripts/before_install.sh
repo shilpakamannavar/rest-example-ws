382836620a8d300cd80f6b30be0323df7e084430
unknown <shilpa.kamannavar@gmail.com>
2021-11-16 08:00:38 +0000

    changes to scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/server
 sudo yum install java-1.8.0-openjdk -y
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
-mvn –version
